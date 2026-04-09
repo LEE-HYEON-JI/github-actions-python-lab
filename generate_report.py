46be70cbacd5c1ab1dab68a30b48fd384bec6cdf
LEE-HYEON-JI <1215lhj@gmain.com>
2026-04-09 15:03:01 +0900

    upload artifact

--- generate_report.py
+++ generate_report.py
@@ -2,5 +2,5 @@ from datatime import datetime
 
 with open("report.txt", "w", encoding="utf-8") as f:
     f.write("Github Actions Report\n")
-    f.write("Generated on: {datetime.now()}\n")
+    f.write(f"Generated on: {datetime.now()}\n")
 print("REPORT.TXT 파일 생성 완료.")
